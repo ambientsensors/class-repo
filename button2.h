8942cc288f85aca33af70eda6cd3445efd4fc6bf
Sandra Capri <sandra.capri@ambientsensors.com>
2022-07-12 21:44:31 -0600

    Added 2 button driver Maybe I should have coordinated with the 10 button driver developer...

---
+++ button2.h
@@ -0,0 +1,4 @@
+// remove from file after int handler testing complete
+int btn_int(void *id, void *state);
+
+int int_attach(uint32_t int_en_bits, int gpio_bank);
--- constants.h
+++ constants.h
@@ -1,2 +1,5 @@
 #define AVALUE (1)
 #define BVALUE (2)
+
+#define BUTTON2_BITS 0x03
+#define BUTTON2_GPIO_INT 0x0
