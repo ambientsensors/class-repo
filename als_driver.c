979fcf25ad847a450550e019be6f77c9100fe5e4
Sandra Capri <scapri@gmail.com>
2022-07-13 21:54:55 -0600

    Fix magic number - from PR review feedback
    Ciaodog asked me to fix - done

--- als_driver.c
+++ als_driver.c
@@ -1,5 +1,6 @@
 #define ALS_EN_GPIO 11
 #define ALS_READ_ADDR 0xA110FF30
+#define FAKE_READ_VALUE 200
 
 
 #include <stdio.h>
@@ -8,7 +9,7 @@
 int als_read_reg(uint32_t als_reg_addr)
 {
     printf("read value of 0x%X from als at addr 0x%X\n",
-           200, ALS_READ_ADDR);
+           FAKE_READ_VALUE, ALS_READ_ADDR);
 
     return 0;
 }
